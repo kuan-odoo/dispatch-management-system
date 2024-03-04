61f4adbb2a5ffb78a6afbc9d162a0ff0dfa6d3e3
Anil kumawat <kuan@odoo.com>
2024-03-04 11:46:15 +0530

    first commit

---
+++ stock_transport/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Stock transport",
+    'version': '1.0',
+    'depends': ['base', 'sale','website'],
+    'author': "Anil kumawat",
+    'category': 'Category',
+    'description': """
+    Description text
+    """,
+    # data files always loaded at installation
+    'data': [
+        
+        
+    ],
+    # # data files containing optionally loaded demonstration data
+    # 'demo': [
+    #     'demo/demo_data.xml',
+    # ],
+    # 'demo': [
+    #     'data/estate_demo.xml',
+    # ],
+    'installable': True,
+    'application': True,
+    
+}
