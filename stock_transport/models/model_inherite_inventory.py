9fabaa8f066f63eecbf98e60da43dcd6bd589166
Anil kumawat <kuan@odoo.com>
2024-03-06 18:44:14 +0530

    added the lable in progress bar and added the thumbnail in gantt chart

--- stock_transport/models/model_inherite_inventory.py
+++ stock_transport/models/model_inherite_inventory.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 
 class InheritInventory(models.Model):
     _inherit = 'stock.picking.batch'
-
+    partner_id = fields.Many2one('res.partner', string='User')
     dock = fields.Many2one('model.dock',string="Dock")
 
     vehicle = fields.Many2one("fleet.vehicle","Vehicle")
@@ -20,9 +20,11 @@ class InheritInventory(models.Model):
     transfers = fields.Float(compute="_compute_transfer",string="Transfer",store=True)
 
     lines = fields.Float(compute="_compute_lines",string="Lines",store=True)
-
-
-    @api.depends('picking_ids.shipping_weight')
+    
+    display_name = fields.Char(compute="_compute_total",store=True,string="Name")
+    total_weight = fields.Float(string="Total weight")
+    total_volume = fields.Float(cstring="Total volume")
+    @api.depends('picking_ids.shipping_weight','move_ids')
     def _compute_weight(self):
         for record in self:
             current_weight = 0
@@ -30,16 +32,17 @@ class InheritInventory(models.Model):
                 current_weight = current_weight + move_id.product_qty*move_id.product_id.weight
             print('asdkfjsadlfjskfdjsdkfjdkfjlksddkasjfdksfjkdsjfksdjfksdjfksdjfkjdkfjkdjfkjdfj')
             print(current_weight)
+            record.total_weight = current_weight
             if record.category_weight >0:
                 record.weight = (current_weight / record.category_weight)*100
             else:
                 record.weight = 1
 
             if record.weight>100:
-                record.weight = 100
+                record.weight = 100 
 
 
-    @api.depends('picking_ids.shipping_volume')
+    @api.depends('picking_ids.shipping_volume','move_ids')
     def _compute_volume(self):
         for record in self:
             current_volume = 0
@@ -47,6 +50,7 @@ class InheritInventory(models.Model):
                 current_volume = current_volume + move_id.product_qty*move_id.product_id.volume
             print('asdkfjsadlfjskfdjsdkfjdkfjlksddkasjfdksfjkdsjfksdjfksdjfksdjfkjdkfjkdjfkjdfj')
             print(current_volume)
+            record.total_volume = current_volume
             if record.category_weight >0:
                 record.volume = (current_volume / record.category_volume)*100
             else:
@@ -66,3 +70,8 @@ class InheritInventory(models.Model):
         for record in self:
             curr = len(record.move_line_ids)
             record.lines = curr
+    
+    @api.depends('total_weight','total_volume')
+    def _compute_total(self):
+        for record in self:
+            record.display_name =  record.name +" : ("+ str(record.total_weight)+" kg ," + str(record.total_volume)+" m^3)"
--- stock_transport/models/modle_inherite_stock_picking.py
+++ stock_transport/models/modle_inherite_stock_picking.py
@@ -5,23 +5,23 @@ class Inherit(models.Model):
     shipping_volume = fields.Float(compute="_compute_volume" ,string="Volume for shipping")
     shipping_weight = fields.Float(compute="_compute_weight" ,string="Weight for shipping")
 
-    @api.depends('product_id.weight', 'product_id.volume','move_line_ids.quantity')
+    @api.depends('product_id.weight', 'product_id.volume','move_ids.move_line_ids.quantity','move_ids')
     def _compute_weight(self):
         for record in self:
             current_weight = 0
             for move_id in record.move_ids:
                 current_weight = current_weight + move_id.product_qty*move_id.product_id.weight
-            print('asdkfjsadlfjskfdjsdkfjdkfjlksddkasjfdksfjkdsjfksdjfksdjfksdjfkjdkfjkdjfkjdfj')
+            print('asdkfjsadlfjskfdjsdkfjdkfjlksddkasjfdksfjkdsjfksdjfksdjfksdjfkjdkfjkdjfkjdfj1')
             print(current_weight)
             record.shipping_weight= current_weight
 
 
-    @api.depends('product_id.weight', 'product_id.volume','move_line_ids.quantity')
+    @api.depends('product_id.weight', 'product_id.volume','move_ids.move_line_ids.quantity','move_ids')
     def _compute_volume(self):
         for record in self:
             current_volume = 0
             for move_id in record.move_ids:
                 current_volume = current_volume + move_id.product_qty*move_id.product_id.volume
-            print('asdkfjsadlfjskfdjsdkfjdkfjlksddkasjfdksfjkdsjfksdjfksdjfksdjfkjdkfjkdjfkjdfj')
+            print('asdkfjsadlfjskfdjsdkfjdkfjlksddkasjfdksfjkdsjfksdjfksdjfksdjfkjdkfjkdjfkjdfj2')
             print(current_volume)
             record.shipping_volume= current_volume
